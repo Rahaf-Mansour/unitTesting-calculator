d178c55bd5b13bd68ebfb4cae2f07d1d9f904d6b
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-09-16 20:54:41 +0300

    Adding more test cases

--- calculator.test.js
+++ calculator.test.js
@@ -51,3 +51,48 @@ describe("calculator", () => {
     expect(() => calc("2", "+", 3)).toThrow("Invalid input type");
   });
 });
+
+// More test cases
+describe("calculator", () => {
+  it("should allow calc to handle an unknown amount of numbers", () => {
+    expect(calc(2, "+", 3, "+", 4)).toBe(9);
+  });
+
+  it("should ignore numbers bigger than 1000", () => {
+    expect(calc(1001, "+", 2)).toBe(2);
+  });
+
+  it("should handle multiple operators correctly", () => {
+    expect(calc(8, "+", 3, "*", 4, "-", 5)).toBe(15);
+  });
+
+  it("should allow calc to handle an unknown amount of numbers with negative result", () => {
+    expect(calc(5, "-", 3, "*", 4)).toBe(-7);
+  });
+
+  it("should throw an error for invalid input types", () => {
+    expect(() => calc("26", "+", "3", "*", 4, "-", 3)).toThrow(
+      "Invalid input type"
+    );
+  });
+
+  it("should ignore the operand with value greater than 1000", () => {
+    expect(calc(2023, "+", 3, "*", 4, "-", 5, "/", 2, "*", 3)).toBe(4.5);
+  });
+
+  it("should result the same inserted value when we insert just one operand", () => {
+    expect(calc(5)).toBe(5);
+  });
+
+  it("should ignore the operand with value greater than 1000", () => {
+    expect(calc(2, "+", 1008, "*", 4, "-", 5, "/", 2, "*", 3)).toBe(-1.5);
+  });
+
+  it("should respect the mathematical order of operations", () => {
+    expect(calc(2, "-", 3, "*", 6, "-", 0, "/", 2, "*", 3)).toBe(-16);
+  });
+
+  it("should ignore the operand with value greater than 1000", () => {
+    expect(calc(2, "+", 3, "*", 4, "-", 1005, "/", 2, "*", 3)).toBe(12.5);
+  });
+});
