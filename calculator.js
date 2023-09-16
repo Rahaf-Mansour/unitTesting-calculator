d8ecb89d9d30333ab7ad0c6fa19eed36ef79771a
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-09-16 20:50:39 +0300

    Adding the calculator.js functionality

--- calculator.js
+++ calculator.js
@@ -1,24 +1,71 @@
-// calculator.js
-function calc(num1, operator, num2) {
-  if (typeof num1 !== "number" || typeof num2 !== "number") {
-    throw "Invalid input type";
-  }
+function calc(...args) {
+  let result;
+  let temp = 0;
+  let operandIndex = 0;
+  let operationIndex = 1;
+  let finalArray = [];
 
-  switch (operator) {
-    case "+":
-      return num1 + num2;
-    case "-":
-      return num1 - num2;
-    case "*":
-      return num1 * num2;
-    case "/":
-      if (num2 === 0) {
-        throw "Division by zero";
+  while (operandIndex < args.length) {
+    //throw error if one of the operands is not a number
+    if (typeof args[operandIndex] !== "number") {
+      throw new Error("Invalid input type");
+    }
+
+    //check if the one of the operand have a value greater than 1000
+    if (
+      args[operandIndex] > 1000 &&
+      (args[operationIndex] === "+" || args[operationIndex] === "-")
+    ) {
+      args[operandIndex] = 0;
+    } else if (
+      args[operandIndex] > 1000 &&
+      (args[operationIndex] === "*" || args[operationIndex] === "/")
+    ) {
+      args[operandIndex] = 1;
+    }
+
+    // check if we reach the last element
+    if (operandIndex === args.length - 1) {
+      finalArray.push(args[operandIndex]);
+      break;
+    }
+
+    // Calculator operations multiplication or division
+    if (args[operationIndex] === "*") {
+      temp = args[operandIndex] * args[operandIndex + 2];
+      args[operationIndex + 1] = temp;
+    } else if (args[operationIndex] === "/") {
+      if (args[operandIndex + 2] === 0) {
+        throw new Error("Division by zero");
       }
-      return num1 / num2;
-    default:
-      throw "Invalid operator";
+      temp = args[operandIndex] / args[operandIndex + 2];
+      args[operationIndex + 1] = temp;
+    }
+    // Calculator operations addition or subtraction
+    else if (args[operationIndex] === "+" || args[operationIndex] === "-") {
+      finalArray.push(args[operandIndex]);
+      finalArray.push(args[operationIndex]);
+    } else {
+      throw new Error("Invalid operator");
+    }
+
+    operationIndex += 2;
+    operandIndex += 2;
   }
+  operandIndex = 0;
+  operationIndex = 1;
+  while (operandIndex < finalArray.length - 1) {
+    if (finalArray[operationIndex] === "+") {
+      temp = finalArray[operandIndex] + finalArray[operandIndex + 2];
+    } else {
+      temp = finalArray[operandIndex] - finalArray[operandIndex + 2];
+    }
+    finalArray[operandIndex + 2] = temp;
+    operandIndex += 2;
+    operationIndex += 2;
+  }
+  result = finalArray[finalArray.length - 1]; // the final result in the last element of the finalArray
+  return result;
 }
 
 module.exports = calc;
