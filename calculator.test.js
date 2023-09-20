e28a39cc5cde97c5c492395fcdc349dff88a5654
Rahaf Mansour <109438456+Rahaf-Mansour@users.noreply.github.com>
2023-09-21 01:54:15 +0300

    Edit the testing describe structure to be more descriptive and clear

--- calculator.test.js
+++ calculator.test.js
@@ -1,98 +1,90 @@
 const calc = require("./calculator");
 
-describe("calculator", () => {
-  // Test case: Addition
-  it("should return the correct sum of two numbers", () => {
+describe("Calculator operations between two numbers", () => {
+  it("Should return 5 when the adding 2 and 3", () => {
     expect(calc(2, "+", 3)).toBe(5);
   });
 
-  // Test case: Subtraction
-  it("should return the correct difference of two numbers", () => {
+  it("Should return 3 when subtract 2 from 5", () => {
     expect(calc(5, "-", 2)).toBe(3);
   });
 
-  // Test case: Multiplication
-  it("should return the correct product of two numbers", () => {
+  it("Should return 24 as a result of a production between 4 and 6", () => {
     expect(calc(4, "*", 6)).toBe(24);
   });
 
-  // Test case: Division
-  it("should return the correct quotient of two numbers", () => {
+  it("Should return 5 when dividing 10 by 2", () => {
     expect(calc(10, "/", 2)).toBe(5);
   });
 
-  // Test case: Division by zero
-  it("should throw an error when dividing by zero", () => {
+  it("Should throw an error when dividing by zero", () => {
     expect(() => calc(6, "/", 0)).toThrow("Division by zero");
   });
 
-  // Test case: Negative numbers
-  it("should handle negative numbers correctly", () => {
+  it("Should return -3 when adding -8 and 5", () => {
     expect(calc(-8, "+", 5)).toBe(-3);
   });
 
-  // Test case: Decimal numbers
-  it("should handle decimal numbers correctly", () => {
+  it("Should handle decimal numbers correctly and give 7 when multiply 3.5 by 2", () => {
     expect(calc(3.5, "*", 2)).toBe(7);
+  });
+
+  it("Should throw an error for an invalid operator like a $", () => {
+    expect(() => calc(5, "$", 3)).toThrow("Invalid operator");
+  });
+
+  it("should throw an error for invalid input types like a string plus sign", () => {
+    expect(() => calc("2", "+", 3)).toThrow("Invalid input type");
   });
 
   // Test case: Order of operations
   it("should follow the correct order of operations", () => {
     expect(calc(2, "+", 3, "*", 4)).toBe(14);
   });
-
-  // Test case: Invalid operator
-  it("should throw an error for an invalid operator", () => {
-    expect(() => calc(5, "$", 3)).toThrow("Invalid operator");
-  });
-
-  // Test case: Invalid input type
-  it("should throw an error for invalid input types", () => {
-    expect(() => calc("2", "+", 3)).toThrow("Invalid input type");
-  });
 });
 
-// More test cases
-describe("calculator", () => {
-  it("should allow calc to handle an unknown amount of numbers", () => {
+describe("Calculator operations for more than two numbers", () => {
+  it("Should return 9 when the operation is adding 2, 3 and 4", () => {
     expect(calc(2, "+", 3, "+", 4)).toBe(9);
   });
 
-  it("should ignore numbers bigger than 1000", () => {
+  it("Should ignore numbers bigger than 1000", () => {
     expect(calc(1001, "+", 2)).toBe(2);
   });
 
-  it("should handle multiple operators correctly", () => {
+  it("Should return 15 when the operation is 8 + 3 * 4 - 5", () => {
     expect(calc(8, "+", 3, "*", 4, "-", 5)).toBe(15);
   });
 
-  it("should allow calc to handle an unknown amount of numbers with negative result", () => {
+  it("Should return -7 when the operation is 5 - 3 * 4", () => {
     expect(calc(5, "-", 3, "*", 4)).toBe(-7);
   });
 
-  it("should throw an error for invalid input types", () => {
+  it("Should throw an error for invalid input types", () => {
     expect(() => calc("26", "+", "3", "*", 4, "-", 3)).toThrow(
       "Invalid input type"
     );
   });
 
-  it("should ignore the operand with value greater than 1000", () => {
+  it("Should ignore the operand with value greater than 1000", () => {
     expect(calc(2023, "+", 3, "*", 4, "-", 5, "/", 2, "*", 3)).toBe(4.5);
   });
 
-  it("should result the same inserted value when we insert just one operand", () => {
+  it("Should result the same inserted value when we insert just one operand", () => {
     expect(calc(5)).toBe(5);
   });
+});
 
-  it("should ignore the operand with value greater than 1000", () => {
+describe("Test for ignoring the operand with value greater than 1000 in different operations", () => {
+  it("Should return -1.5 for the operation 2 + 1008 * 4 - 5 / 2 * 3", () => {
     expect(calc(2, "+", 1008, "*", 4, "-", 5, "/", 2, "*", 3)).toBe(-1.5);
   });
 
-  it("should respect the mathematical order of operations", () => {
+  it("Should respect the mathematical order of operations and return -16 for 2 - 3 * 6 - 0 / 2 * 3", () => {
     expect(calc(2, "-", 3, "*", 6, "-", 0, "/", 2, "*", 3)).toBe(-16);
   });
 
-  it("should ignore the operand with value greater than 1000", () => {
+  it("Should return 12.5 for the operation 2 + 3 * 4 - 1005 / 2 * 3", () => {
     expect(calc(2, "+", 3, "*", 4, "-", 1005, "/", 2, "*", 3)).toBe(12.5);
   });
 });
